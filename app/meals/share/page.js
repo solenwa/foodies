9c6218dc2d7e580c9337185964bc38c53c1c1476
solenwa <solen.wanono@gmail.com>
2024-12-17 15:16:07 +0100

    Fine tuning the form

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,7 +1,14 @@
-import ImagePicker from '@/components/meals/image-picker';
+'use client';
+
 import classes from './page.module.css';
+import ImagePicker from '@/components/meals/image-picker';
+import MealsFormSubmit from '@/components/meals/meals-form-submit';
+import { shareMeal } from '@/lib/actions';
+import { useFormState } from 'react-dom';
 
 export default function ShareMealPage() {
+  const [state, formAction] = useFormState(shareMeal, { message: null });
+
   return (
     <>
       <header className={classes.header}>
@@ -11,7 +18,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -39,9 +46,10 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker />
+          <ImagePicker label="Your image" name="image" />
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
